acce610edd9acb1720695b694d9a9620125b89dd
Himanshu18078 <himanshubisht18078@gmail.com>
2026-06-13 14:19:59 +0530

    initial changes

---
+++ Object Oriented Programming/7 String Class/StringBuilder Class/src/StringBuilder1.java
@@ -0,0 +1,10 @@
+public class StringBuilder1 {
+    public static void main(String [] args) {
+        StringBuilder sb = new StringBuilder("First");
+        sb.append(45);
+        sb.append("ho are you...");
+        sb.append(766);
+//        sb.toString();
+        System.out.println(sb);
+    }
+}
