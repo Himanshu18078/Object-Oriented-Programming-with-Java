6c529b4a95c6af0dccf66c0ecb7d1fa71f9ab6ca
Himanshu18078 <himanshubisht18078@gmail.com>
2026-06-13 14:16:59 +0530

    final keyword

---
+++ Object Oriented Programming/7 String Class/8 Final Keyword/src/FinalKeyword.java
@@ -0,0 +1,6 @@
+public class FinalKeyword {
+    public final String NAME = "Himanshu";
+    public static void main(){
+//        FinalKeyword.name = "hello"; //java: cannot assign a value to final variable name
+    }
+}
