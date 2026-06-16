27ee3f3b4d77d17a054fda35a42582882c2f4068
Himanshu18078 <himanshubisht18078@gmail.com>
2026-06-16 14:15:42 +0530

    Mthod chaining improved

--- Object Oriented Programming/2 This Keyword/src/MethodChaining/Driver.java
+++ Object Oriented Programming/2 This Keyword/src/MethodChaining/Driver.java
@@ -1,13 +1,22 @@
-package MethodChaining;
-
 public class Driver {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Car swift = new Car();
         swift.addFuel(6);
-        swift.start().drive(); // This is call method chaining
-//        “I created a swift object and added fuel to it.
-//        When I call start(), the method returns the same MethodChaining.Car object (using return this).
-//        This returned reference can be treated like a new variable that points to the same object,
-//        allowing me to immediately call other methods like drive() on it. That is why method chaining works.”
+
+        swift.start().drive(); // Method chaining
+        /*
+         * The start() method returns 'this', which is a reference to the
+         * current object. The returned reference points to the same Car
+         * object in memory as 'swift'.
+         *
+         * Since the returned reference is of type Car, we can immediately
+         * call another method such as drive() on it.
+         *
+         * This is why method chaining works:
+         * swift.start().drive();
+         *
+         * A Java reference variable does not store the object's data (fields).
+         * It stores only a reference to the object.
+         */
     }
 }
